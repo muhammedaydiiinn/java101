93fb58eca60e61726f39f976eb6b1ffae23614e5
Muhammed Aydın <112319140+muhammedaydiiinn@users.noreply.github.com>
2023-03-13 12:27:58 +0300

    Add files via upload

---
+++ Transpose.java
@@ -0,0 +1,29 @@
+public class Transpose {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6}
+        };
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        System.out.println("Matrix : ");
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[i].length;j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Transpose : ");
+        for(int i=0; i<transpose.length; i++) {
+            for(int j=0; j<transpose[i].length;j++) {
+                transpose[i][j] = matrix[j][i];
+                System.out.print(transpose[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
